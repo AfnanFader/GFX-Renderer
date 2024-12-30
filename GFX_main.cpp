f8db4338ac03db1b4a6c8dbfe2adcf23bfedcb9d
AfnanFader <afnanfader@gmail.com>
2024-12-31 04:58:27 +0800

    setting up build tools

---
+++ GFX_main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @ Author: Afnan Fader
+ * @ Create Time: 2024-12-31 04:09:07
+ * @ Description: GFX Renderer Main Functions
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    cout << "Hello !" << endl;
+
+    return 0;
+}
